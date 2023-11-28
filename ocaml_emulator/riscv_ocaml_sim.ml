2eb29488983be2d91b9b94cb7f5903ffd73f7bd6
Lowie Deferme <lowie.deferme@kuleuven.be>
2023-11-28 17:05:13 +0100

    refactor: use - instead of _ for ocaml sim flag

--- ocaml_emulator/riscv_ocaml_sim.ml
+++ ocaml_emulator/riscv_ocaml_sim.ml
@@ -53,7 +53,7 @@ let options = Arg.align ([("-dump-dts",
                           ("-mtval-has-illegal-inst-bits",
                            Arg.Set P.config_mtval_has_illegal_inst_bits,
                            " mtval stores instruction bits on an illegal instruction exception");
-                          ("-xtinst_has_transformed_inst",
+                          ("-xtinst-has-transformed-inst",
                            Arg.Set P.config_xtinst_has_transformed_inst,
                            " xtinst stores tranformed instruction bits");
                           ("-disable-rvc",
